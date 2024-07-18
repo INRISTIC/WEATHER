0ca4ec75542c1719c991acf0d5c4f80a7011368f
v.dyakov <v.dyakov@sbereducation.ru>
2024-07-19 02:32:51 +0300

    chore

--- src/utils/convertDate.ts
+++
@@ -1,5 +0,0 @@
-import { format, fromUnixTime } from "date-fns";
-
-export const convertDate = (unixTimestamp: number): string => {
-  return format(fromUnixTime(unixTimestamp), 'hh:mma').toLowerCase();
-};
